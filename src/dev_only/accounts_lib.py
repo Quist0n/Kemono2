cdf84d7d4838b81ddbcb1fa413136e8631d216a0
Quist0n <37319086+Quist0n@users.noreply.github.com>
2021-08-24 12:11:57 -0500

    feat(server): Created functions to save test user data to json and save to a test folder.

--- src/dev_only/accounts_lib.py
+++ src/dev_only/accounts_lib.py
@@ -1,9 +1,11 @@
 import string
+import json
+import os
 
 from random import Random
 from datetime import datetime
 
-# from configs.constants import test_path
+from configs.constants import test_path
 
 from typing import Dict, List
 
@@ -11,9 +13,9 @@ acc_random = Random('Sneed')
 username_vocabulary = string.ascii_letters + string.digits
 password_vocabulary = string.ascii_letters + string.digits + string.punctuation
 
-def make_test_accounts() -> List[Dict]:
-    accounts = [generate_creds() for item in range(1000)]
-    
+def make_test_accounts(count = 1000) -> List[Dict]:
+    accounts = [generate_creds() for item in range(count)]
+
     return accounts
 
 def generate_creds() -> Dict[str, str]:
@@ -29,9 +31,14 @@ def generate_random_string(min_length: int, max_length: int, vocabulary: str, ra
 
     return result_string
 
-def create_accounts_json(accounts: List[Dict]) -> None:
-    pass
+def create_accounts_json(accounts: List[Dict]) -> str:
+    return json.dumps(accounts, indent=4)
 
-test_accounts = make_test_accounts()
-print(test_accounts[0], test_accounts[1], test_accounts[2])
-    
+def write_test_accounts_data(account_data: str) -> None:
+    output_file = str(test_path) + "/accounts.json"
+    if os.path.isdir(test_path) == False:
+        os.mkdir(test_path)
+
+    with open(output_file, 'wt') as f:
+        f.write(create_accounts_json(account_data))
+        f.close()
