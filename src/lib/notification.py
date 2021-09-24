3cf00b4ff73f23e103a5ce0de88ae4a3f4153b4c
Quist0n <37319086+Quist0n@users.noreply.github.com>
2021-09-23 23:29:12 -0500

    feat(cache): Cache notifications in redis when they retrieved from DB

--- src/lib/notification.py
+++ src/lib/notification.py
@@ -1,7 +1,11 @@
-from json import dumps
-from src.internals.database.database import get_cursor
+import copy
+import dateutil
+import ujson
 
 from typing import Dict, List, Optional
+
+from src.internals.cache.redis import get_conn
+from src.internals.database.database import get_cursor
 
 from src.types.account import Notification
 
@@ -12,7 +16,7 @@ def count_account_notifications(account_id: int) -> int:
 
     cursor = get_cursor()
     query = """
-        SELECT 
+        SELECT
             COUNT(*) AS notifications_count
         FROM notification
         WHERE account_id = %(account_id)s
@@ -22,30 +26,38 @@ def count_account_notifications(account_id: int) -> int:
     notifications_count = result["notifications_count"]
     return notifications_count
 
-def get_account_notifications(account_id: int):
-    """
-    TODO: caching
-    """
-    args_dict = {
-        "account_id": account_id
-    }
+def get_account_notifications(account_id: int, reload: bool = False) -> List[Notification]:
+    redis = get_conn()
+    key = f"notifications_for_account:{account_id}"
+    notifications = redis.get(key)
 
-    cursor = get_cursor()
-    query = """
-        SELECT id, account_id, type, created_at, extra_info
-        FROM notification
-        WHERE account_id = %(account_id)s
-        ORDER BY
-            created_at DESC
-    """
-    cursor.execute(query, args_dict)
-    result = cursor.fetchall()
-    notifications = [Notification.init_from_dict(notification) for notification in result]
+    if notifications is None or reload:
+        args_dict = {
+            "account_id": account_id
+        }
+
+        cursor = get_cursor()
+        query = """
+            SELECT id, account_id, type, created_at, extra_info
+            FROM notification
+            WHERE account_id = %(account_id)s
+            ORDER BY
+                created_at DESC
+        """
+        cursor.execute(query, args_dict)
+        result = cursor.fetchall()
+
+        notifications = [Notification.init_from_dict(notification) for notification in result]
+        redis.set(key, serialize_notifications(result), ex = 60)
+
+    else:
+        notifications = deserialize_notifications(notifications)
+
     return notifications
 
 def send_notifications(
-    account_ids: List[str], 
-    notification_type: int, 
+    account_ids: List[str],
+    notification_type: int,
     extra_info: Optional[Dict[str,str]]
 ) -> bool:
     cursor = get_cursor()
@@ -54,17 +66,44 @@ def send_notifications(
         return False
 
     if extra_info is not None:
-        extra_info = dumps(extra_info)
-        notification_values = f'(%s, {notification_type}, \'{extra_info}\')'
+        extra_info = ujson.dumps(extra_info)
+        notification_values = f"(%s, {notification_type}, '{extra_info}')"
     else:
-        notification_values = f'(%s, {notification_type}, NULL)'
+        notification_values = f"(%s, {notification_type}, NULL)"
 
     insert_queries_values_template = ",".join([notification_values] * len(account_ids))
     insert_query = f"""
-        INSERT INTO notification (account_id, type, extra_info) 
+        INSERT INTO notification (account_id, type, extra_info)
         VALUES {insert_queries_values_template}
         ;
         """
     cursor.execute(insert_query, account_ids)
 
     return True
+
+def serialize_notification(notification):
+    if notification is not None:
+        notification = prepare_notification_fields(copy.deepcopy(notification))
+    return ujson.dumps(notification)
+
+def deserialize_notification(notification_str):
+    notification = ujson.loads(notification_str)
+    if notification is not None:
+        notification = rebuild_notification_fields(notification)
+    return notification
+
+def serialize_notifications(notifications):
+    notifications = copy.deepcopy(notifications)
+    return ujson.dumps(list(map(lambda notification: prepare_notification_fields(notification), notifications)))
+
+def deserialize_notifications(notifications_str):
+    notifications = ujson.loads(notifications_str)
+    return list(map(lambda notification: rebuild_notification_fields(notification), notifications))
+
+def prepare_notification_fields(notification):
+    notification['created_at'] = notification['created_at'].isoformat()
+    return notification
+
+def rebuild_notification_fields(notification):
+    notification['created_at'] = dateutil.parser.parse(notification['created_at'])
+    return notification
