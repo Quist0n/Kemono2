db5492645d37cd88b35dc1176ed6a823d62315ab
Quist0n <37319086+Quist0n@users.noreply.github.com>
2022-03-07 19:25:43 -0500

    refactor(api): create /api/v1/artists endpoint

--- src/blueprints/api/v1/blueprint.py
+++ src/blueprints/api/v1/blueprint.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request, make_response, jsonify
 
 from src.lib.account import load_account
-from src.utils.utils import get_value
+from src.utils.utils import get_value, parse_int
 from src.lib.favorites import get_favorite_artists, get_favorite_posts
+from src.internals.database.database import get_cursor
 
 v1api = Blueprint('v1', __name__, url_prefix='/v1')
 
@@ -23,4 +24,25 @@ def list_account_favorites():
     results = favorites
     response = make_response(jsonify(results), 200)
     response.headers['Cache-Control'] = 's-maxage=60'
+    return response
+
+
+@v1api.get("/artists")
+def list():
+    limit = parse_int(request.args.get('limit'), None) if request.args.get('limit') else None
+    if (limit is None):
+        query = "SELECT id, indexed, name, service, updated FROM lookup WHERE service != 'discord-channel'"
+        cursor = get_cursor()
+        cursor.execute(query)
+        results = cursor.fetchall()
+    else:
+        arg_dict = {'limit': limit}
+        query = "SELECT id, indexed, name, service, updated FROM lookup WHERE service != 'discord-channel' LIMIT %(limit)s"
+        cursor = get_cursor()
+        cursor.execute(query, arg_dict)
+        results = cursor.fetchall()
+
+    response = make_response(jsonify(results), 200)
+    response.headers['Cache-Control'] = 's-maxage=60'
+
     return response
